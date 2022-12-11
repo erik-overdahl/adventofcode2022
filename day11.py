0013d4e01aab6f5a688f09a3c257445fe7766ff6
Erik Overdahl <erik.overdahl@gmail.com>
2022-12-11 14:33:49 -0600

    Day 11: Learn math, complete part 2

--- day11.py
+++ day11.py
@@ -9,7 +9,7 @@ class Monkey:
         self.test = test
         self.neighbors = neighbors
         self.items = initial_items
-        self.ptr = 0
+        self.seen = 0
 
     def inspect(self, item: int) -> int:
         return self.op(item)
@@ -23,16 +23,12 @@ def parse_monkey(input: list[str]) -> Monkey:
     op_line = input[2].split(" ")
     arg = op_line[-1]
     op = operator.add # sensible default?
-    match op_line[-2]:
-        case "+":
-            op = operator.add
-        case "-":
-            op = operator.sub
-        case "*":
-            op = operator.mul
-        case "/":
-            op = operator.floordiv
-    f = lambda x: op(x, x if arg == "old" else int(arg))
+    if arg == "old":
+        op = operator.pow
+    elif op_line[-2] == "*":
+        op = operator.mul
+    a = 2 if arg == "old" else int(arg)
+    f = lambda x,const=a: op(x, const)
     test = int(input[3].split(" ")[-1])
     neighbors = (int(input[4].split(" ")[-1]), int(input[5].split(" ")[-1]))
     return Monkey(f, test, neighbors, items)
@@ -41,62 +37,35 @@ def parse_monkey(input: list[str]) -> Monkey:
 def business_level(monkeys: list[Monkey]) -> int:
     busiest = [0,0]
     for m in monkeys:
-        if m.ptr > busiest[0]:
-            busiest = [m.ptr, busiest[0]]
-        elif m.ptr > busiest[1]:
-            busiest[1] = m.ptr
+        if m.seen > busiest[0]:
+            busiest = [m.seen, busiest[0]]
+        elif m.seen > busiest[1]:
+            busiest[1] = m.seen
     return busiest[0] * busiest[1]
 
 
-def simulate(input: str, rounds: int, worry_divisor: int) -> int:
-    monkeys = []
-    for monkey_desc in input.split("\n\n"):
-        monkeys.append(parse_monkey(monkey_desc.split("\n")))
-
+def simulate(monkeys: list[Monkey], rounds: int, adjust: Callable[[int], int]) -> int:
     for round in range(rounds):
         for m in monkeys:
-            start = m.ptr
-            for item in m.items[start:]:
-                new = m.inspect(item) // worry_divisor
+            for item in m.items:
+                new = adjust(m.inspect(item))
                 monkeys[m.next(new)].items.append(new)
-            m.ptr = len(m.items)
+            m.seen += len(m.items)
+            m.items = []
 
     return business_level(monkeys)
 
 
-
 input = open("./day11.input").read()
 
-input = """ Monkey 0:
-  Starting items: 79, 98
-  Operation: new = old * 19
-  Test: divisible by 23
-    If true: throw to monkey 2
-    If false: throw to monkey 3
+monkeys = [parse_monkey(desc.split("\n")) for desc in input.split("\n\n")]
+part1 = simulate(monkeys, 20, lambda worry: worry // 3)
 
-Monkey 1:
-  Starting items: 54, 65, 75, 74
-  Operation: new = old + 6
-  Test: divisible by 19
-    If true: throw to monkey 2
-    If false: throw to monkey 0
+monkeys = [parse_monkey(desc.split("\n")) for desc in input.split("\n\n")]
+lcm = 1
+for m in monkeys:
+    lcm *= m.test
 
-Monkey 2:
-  Starting items: 79, 60, 97
-  Operation: new = old * old
-  Test: divisible by 13
-    If true: throw to monkey 1
-    If false: throw to monkey 3
+part2 = simulate(monkeys, 10000, lambda worry: worry % lcm)
 
-Monkey 3:
-  Starting items: 74
-  Operation: new = old + 3
-  Test: divisible by 17
-    If true: throw to monkey 0
-    If false: throw to monkey 1
-"""
-
-part1 = simulate(input, 20, 3)
-part2 = -1
-# part2 = simulate(input, 10000, 1)
 print(f"Part 1: {part1}, Part 2: {part2}")
