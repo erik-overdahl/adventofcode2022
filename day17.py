e8029774e6dd6eab56654990f74e6e2cd2d56360
Erik Overdahl <erik.overdahl@gmail.com>
2022-12-19 10:40:31 -0600

    Day 17: Part 2: Look for cycles

--- day17.py
+++ day17.py
@@ -11,7 +11,14 @@ def print_rock(rock: int):
         print(f"{(rock >> (4-i)*8) & 0xFF:08b}".replace("0",".").replace("1","#"))
 
 
-def drop_rocks(count: int, jets: str) -> list[int]:
+def shift(direction: str, rock:int, row_mask: int) -> int:
+    s = rock >> 1 if direction == ">" else rock << 1
+    if not s & row_mask:
+        return s
+    return rock
+
+
+def drop_rocks(count: int, jets: str) -> int:
     rocks = [
         # 00000000
         # 00000000
@@ -39,47 +46,69 @@ def drop_rocks(count: int, jets: str) -> list[int]:
         # 00110000
         int.from_bytes([0x00,0x00,0x30,0x30]),
     ]
-    rows = [0x01, 0x01, 0x01, 0x01]
+    rows = []
+    # sometimes I hate pyright
+    seen: list[tuple[int,int]] = [(0,0) for _ in range(len(rocks)*len(jets))]
+    last_jet = len(jets) - 1
     jet = 0
-    pos = 3
+    pos = 0
+    top = 0
+
     for r in range(count):
-        rock = rocks[r % 5]
+        i = r % 5
+        rock = rocks[i]
+        sig = jet*5 + i
+        # if we have seen this rock/specific jet combo before, and the
+        # number of rocks dropped between then and now fits evenly into
+        # the number of rocks we have remaining
+        if seen[sig] != (0,0):
+            cycle_start, prev_top = seen[sig]
+            cycle_len = r - cycle_start
+            repeats, offset = divmod(count - r, cycle_len)
+            if offset == 0:
+                return top + (top - prev_top)*repeats
+        seen[sig] = (r, top)
+
+        # every dropped rock gets 3 shifts
         row_mask = 0x01_01_01_01
+        rock = shift(jets[jet], rock, row_mask)
+        jet = jet + 1 if jet < last_jet else 0
+        rock = shift(jets[jet], rock, row_mask)
+        jet = jet + 1 if jet < last_jet else 0
+        rock = shift(jets[jet], rock, row_mask)
+        jet = jet + 1 if jet < last_jet else 0
+
         while True:
             # shift if not blocked
-            d = jets[jet]
-            s = rock >> 1 if d == ">" else rock << 1
-            if not s & row_mask:
-                rock = s
-
-            jet += 1
-            if jet >= len(jets):
-                jet = 0
+            rock = shift(jets[jet], rock, row_mask)
+            jet = jet + 1 if jet < last_jet else 0
 
             if pos == 0 or (rock & ((row_mask << 8) | rows[pos - 1])):
                 break
             pos -= 1
-            # print(f"rock falls to {pos}")
             row_mask = (row_mask << 8) | rows[pos]
 
         for i in range(4):
             row = pos + i
             val = rock & 0xFF
+            if val == 0:
+                break
             if row < len(rows):
                 rows[row] |= val
             else:
                 rows.append(val | 1)
+            for c in range(1,8):
+                if rows[row] & (1 << c):
+                    top = max(row+1, top)
             rock >>= 8
 
-        while rows[-3:] != [0x01,0x01,0x01]:
-            rows.append(0x01)
-
         pos = len(rows)
-    return rows
+    return top
 
 
 jets = open("./day17.input").read().rstrip("\n")
 # jets = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
 
-board = drop_rocks(2022,jets)
-print(len(board)-3)
+part1 = drop_rocks(2022,jets)
+part2 = drop_rocks(int(1e12),jets)
+print(f"Part 1: {part1}, Part 2: {part2}")
